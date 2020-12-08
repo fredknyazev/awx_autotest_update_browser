2bf496dbcb0f83d0778432e952a3788486addf34
ua.knyazev <ua.knyazev@tensor.ru>
2020-12-08 20:27:42 +0300

    update script

--- roles/browserupd/library/win_update_browser.ps1
+++ roles/browserupd/library/win_update_browser.ps1
@@ -8,7 +8,7 @@ $ololo = Import-Module Ansible.ModuleUtils.Legacy -Force
 $params = Parse-Args $args;
 $result = @{};
 Set-Attr $result "changed" $false;
-$state = Get-Attr -obj $params -name state -failifempty $true -emptyattributefailmessage "missing required argument: state"
+$action = Get-Attr -obj $params -name action -failifempty $true -emptyattributefailmessage "missing required argument: action"
 $ffinstall = Get-Attr -obj $params -name ffinstall -default 0
 $chromeinstall = Get-Attr -obj $params -name chromeinstall -default 0
 
@@ -34,12 +34,10 @@ function Get-MsiProductVersion {
     $Installer = New-Object -ComObject WindowsInstaller.Installer
     $Database = Invoke-Method $Installer OpenDatabase  @($Path, $msiOpenDatabaseModeReadOnly)
     $View = Invoke-Method $Database OpenView  @("SELECT Value FROM Property WHERE Property='ProductVersion'")
-    #$View = Invoke-Method $Database OpenView  @("SELECT Value FROM Property WHERE Property='Comments'")
     Invoke-Method $View Execute
     $Record = Invoke-Method $View Fetch
     if ($Record) {
         Write-Output (Get-Property $Record StringData 1)
-        #Write-Output (Get-Property $Record StringData)
     }
     Invoke-Method $View Close @()
     Remove-Variable -Name Record, View, Database, Installer
@@ -49,12 +47,10 @@ Function Kill-Process {
   $processlist = @() 
   $processlist += (Get-Process -name $processname -ErrorAction SilentlyContinue | where {-not $_.HasExited})
   if ($processlist){
-    #Get-Process -name $processname | where {-not $_.HasExited} | Stop-Process -Force -ErrorAction SilentlyContinue
     $exe = $processname + ".exe"
     $taskkill = "C:\Windows\System32\taskkill.exe"
     $params = "/im $exe /f"
     $params1 = $params.Split(" ")
-    #$qq = & "$taskkill" $params1
     Start-Process $taskkill -ArgumentList $params1 -Wait -NoNewWindow
     start-sleep -Seconds 10
     $processlist = @() 
@@ -83,28 +79,37 @@ Function Disable-FF-AutoUpdate {
     $result.changed = $true
   }
 }
+Function Disable-Chrome-AutoUpdate {
+  $Key = "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Google\Update"
+  if  (-Not ( Test-Path "Registry::$Key")){
+    New-Item -Path "Registry::$Key" -ItemType RegistryKey -Force
+  }
+  if ((Get-ItemProperty -Path "Registry::$Key" | select -ExpandProperty UpdateDefault) -eq 0){
+    $result.changed = $false
+  }
+  else {
+    Set-ItemProperty -path "Registry::$Key" -Name "UpdateDefault" -Type DWord -Value 0
+    $result.changed = $true
+  }
+}
 
 Function Install-FF {
   $msg = @{}
   $browserexe = Get-Attr -obj $params -name browserexe -default 'C:\Program Files\Mozilla Firefox\firefox.exe'
   if (test-path $browserexe){
-    #$ffversion = ((get-item $browserexe).VersionInfo.ProductVersion).Split(".")[0]
     $ffversion = [system.version]::Parse((get-item $browserexe).VersionInfo.ProductVersion)
   }
   else {
     $ffversion = [system.version]::Parse("0.0")
   }
   $msg.currentversion = $ffversion.ToString()
-  $copypath = "C:\windows\temp"
   $browserdistr = Get-Attr -obj $params -name browserdistr -failifempty $browserdistr -emptyattributefailmessage "missing required argument: browserdistr"
-  $copydistr = Copy-Item -Path $browserdistr -Destination "C:\Windows\Temp" -PassThru
-  if (-not $copydistr.fullname){
-    Fail-Json $result "failed to copy distr from $browserdistr to $copydistr"
-  }
-  $browserdistr = $copydistr.fullname
-  
   if (test-path $browserdistr){
-    #$distrversion = ((get-item $browserdistr | select -ExpandProperty BaseName).Split(" ")[-1]).Split(".")[0]
+    $copydistr = Copy-Item -Path $browserdistr -Destination "C:\Windows\Temp" -PassThru
+    if (-not $copydistr.fullname){
+      Fail-Json $result "failed to copy distr from $browserdistr to $copydistr"
+    }
+    $browserdistr = $copydistr.fullname
     $distrversion = [system.version]::Parse((get-item $browserdistr | select -ExpandProperty BaseName).Split(" ")[-1])
   }
   else {
@@ -114,10 +119,13 @@ Function Install-FF {
     $params = "/S /MaintenanceService=false"
     $params1 = $params.Split(" ")
     & "$browserdistr" $params1
-    Start-Sleep -Seconds 20
+    $repeats = 0
+    while (($ffversion -ne $distrversion) -and ($repears -le 10)) {
+        Start-Sleep -Seconds 5
+        $ffversion = [system.version]::Parse((get-item $browserexe).VersionInfo.ProductVersion)
+        $repeats += 1
+    }
     Remove-Item -Path $copydistr.fullname -Force
-    #$ffversion = ((get-item $browserexe).VersionInfo.ProductVersion).Split(".")[0]
-    $ffversion = [system.version]::Parse((get-item $browserexe).VersionInfo.ProductVersion)
     if ($ffversion -eq $distrversion){
       $result.changed = $true
     }
@@ -130,30 +138,25 @@ Function Install-FF {
   }
   $msg.newversion = $ffversion.ToString()
   $msg.distrversion = $distrversion.ToString()
-  #$result.msg = ($msg | ConvertTo-Json | % {[Regex]::Replace($_, "\\u(?<Value>[a-zA-Z0-9]{4})", {param($m) ([char]([int]::Parse($m.Groups['Value'].Value, [System.Globalization.NumberStyles]::HexNumber))).ToString()})}).Replace('\\n','\n').Replace('\\r','\r')
   $result.msg = $msg
 }
 Function Install-Chrome{
   $msg = @{}
   $browserexe = Get-Attr -obj $params -name browserexe -default 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
   if (test-path $browserexe){
-    #$chromeversion = ((get-item $browserexe).VersionInfo.ProductVersion).Split(".")[0]
     $chromeversion = [system.version]::Parse((get-item $browserexe).VersionInfo.ProductVersion)
   }
   else {
     $chromeversion = [system.version]::Parse("0.0")
   }
   $msg.currentversion = $chromeversion.ToString()
-  #$browserdistr = Get-Attr -obj $params -name browserdistr -failifempty $browserdistr -emptyattributefailmessage "missing required argument: browserdistr"
-  $copypath = "C:\windows\temp"
   $browserdistr = Get-Attr -obj $params -name browserdistr -failifempty $browserdistr -emptyattributefailmessage "missing required argument: browserdistr"
-  $copydistr = Copy-Item -Path $browserdistr -Destination "C:\Windows\Temp" -PassThru
-  if (-not $copydistr.fullname){
-    Fail-Json $result "failed to copy distr from $browserdistr to $copydistr"
-  }
-  $browserdistr = $copydistr.fullname
   if (test-path $browserdistr){
-    #$distrmsiversion = (Get-MsiProductVersion -Path $browserdistr | where {$_}).Split(".")[0]
+    $copydistr = Copy-Item -Path $browserdistr -Destination "C:\Windows\Temp" -PassThru
+    if (-not $copydistr.fullname){
+      Fail-Json $result "failed to copy distr from $browserdistr to $copydistr"
+    }
+    $browserdistr = $copydistr.fullname
     $path1 = $browserdistr
     $shell = New-Object -COMObject Shell.Application
     $folder = Split-Path $path1
@@ -164,7 +167,6 @@ Function Install-Chrome{
     $comments = @()
     $comments += ($a | where {($_.Contains("Комментарии")) -or ($_.Contains("Comments"))})
     $commentstringnumber = ($comments[0].Split("="))[0].Trim()
-    #$distrversion = (($shellfolder.GetDetailsOf($shellfile, $commentstringnumber)).Split("."))[0]
     $distrversion = [system.version]::Parse((($shellfolder.GetDetailsOf($shellfile, $commentstringnumber)).Split(" "))[0])
   }
   else {
@@ -178,12 +180,14 @@ Function Install-Chrome{
       "/qn"
       "/norestart"
     )
-    #$params1 = $params.Split(" ")
     Start-Process msiexec.exe -ArgumentList $MSIArguments -Wait -NoNewWindow
-    Start-Sleep -Seconds 20
+    $repeats = 0
+    while (($chromeversion -ne $distrversion) -and ($repears -le 10)) {
+        Start-Sleep -Seconds 5
+        $chromeversion = [system.version]::Parse((get-item $browserexe).VersionInfo.ProductVersion)
+        $repeats += 1
+    }
     Remove-Item -Path $copydistr.fullname -Force
-    #$chromeversion = ((get-item $browserexe).VersionInfo.ProductVersion).Split(".")[0]
-    $chromeversion = [system.version]::Parse((get-item $browserexe).VersionInfo.ProductVersion)
     if ($chromeversion -eq $distrversion){
       $result.changed = $true
     }
@@ -195,21 +199,60 @@ Function Install-Chrome{
     $result.changed = $false
   }
   $msg.newversion = $chromeversion.ToString()
-  #$msg.browserexe = $browserexe
-  #$msg.browserdistr = $browserdistr
   $msg.distrversion = $distrversion.ToString()
-  #$msg.msiversion = $distrmsiversion
-  #$result.msg = ($msg | ConvertTo-Json | % {[Regex]::Replace($_, "\\u(?<Value>[a-zA-Z0-9]{4})", {param($m) ([char]([int]::Parse($m.Groups['Value'].Value, [System.Globalization.NumberStyles]::HexNumber))).ToString()})}).Replace('\\n','\n').Replace('\\r','\r')
   $result.msg = $msg
 }
+Function Copy-Webdriver{
+  $webdriver = Get-Attr -obj $params -name webdriver -default $null
+  $webdriver_path = Get-Attr -obj $params -name webdriver_path -default $null
+  if ($webdriver -ne $null -and $webdriver_path -ne $null) {
+      $driver_name = Split-Path $webdriver -leaf
+      $old_driver_name = "$($webdriver_path)\old_$($driver_name)"
+      if (test-path $old_driver_name)
+      {
+        Remove-Item $old_driver_name
+      }
+      $webdriver_file_path = "$($webdriver_path)\$($driver_name)"
+      if (test-path $webdriver_file_path)
+      {
+        Rename-Item $webdriver_file_path -NewName "old_$($driver_name)" -Force
+      }
+      $processname = ""
+      if ($chromeinstall -eq 1)
+      {
+        $processname = "chromedriver"
+      }
+      if ($ffinstall -eq 1)
+      {
+        $processname = "geckodriver"
+      }
+      $processlist = @()
+      $processlist += (Get-Process -name $processname -ErrorAction SilentlyContinue | where {-not $_.HasExited})
+      if ($processlist){
+        $exe = $processname + ".exe"
+        $taskkill = "C:\Windows\System32\taskkill.exe"
+        $params = "/im $exe /f"
+        $params1 = $params.Split(" ")
+        Start-Process $taskkill -ArgumentList $params1 -Wait -NoNewWindow
+        start-sleep -Seconds 10
+        $processlist = @()
+        $processlist += (Get-Process -name $processname -ErrorAction SilentlyContinue | where {-not $_.HasExited})
+        if ($processlist){
+          Fail-Json $result "failed to kill $processname"
+        }
+        }
+      Copy-Item $webdriver -Destination $webdriver_path -Force
+    }
 Try {
-  switch ($state) {
-    kill {Kill-Process}
-    ffinstall {if ($ffinstall -eq 1){Install-FF}}
-    chromeinstall {if ($chromeinstall -eq 1){Install-Chrome}}
+  switch ($action) {
+    Kill {Kill-Process}
+    FFInstall {if ($ffinstall -eq 1){Install-FF}}
+    ChromeInstall {if ($chromeinstall -eq 1){Install-Chrome}}
     FFUpdatesDisabled {if ($ffinstall -eq 1){Disable-FF-AutoUpdate}}
-    ffinstallFull {if ($ffinstall -eq 1){kill-Process; Disable-FF-AutoUpdate; Install-FF}}
-    cromeinstallFull {if ($chromeinstall -eq 1){Kill-Process; Install-Chrome}}
+    ChromeUpdatesDisabled {if ($chromeinstall -eq 1){Disable-Chrome-AutoUpdate}}
+    FFInstallFull {if ($ffinstall -eq 1){Kill-Process; Disable-FF-AutoUpdate; Install-FF; Copy-Webdriver}
+    ChromeInstallFull {if ($chromeinstall -eq 1){Kill-Process; Install-Chrome; Copy-Webdriver; Disable-Chrome-AutoUpdate}}
+    CopyWebdriver {if (($chromeinstall -eq 1) or($ffinstall -eq 1)) Copy-Webdriver}
   }
   Exit-Json $result;
 }
