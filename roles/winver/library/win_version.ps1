0948d3fb6a93c4fd870f50b495dd682fa0a812aa
ua.knyazev <ua.knyazev@tensor.ru>
2020-12-26 11:34:10 +0300

    winver script

--- roles/winver/library/win_version.ps1
+++ roles/winver/library/win_version.ps1
@@ -6,12 +6,27 @@
 
 $result = @{};
 Try {
+  $WindowsProductName = Get-Attr -obj $params -name WindowsProductName
+  $WindowsVersion = Get-Attr -obj $params -name WindowsVersion
+  $OsHardwareAbstractionLayer = Get-Attr -obj $params -name OsHardwareAbstractionLayer
   $infoversion = Get-ComputerInfo | select WindowsProductName, WindowsVersion, OsHardwareAbstractionLayer
   $msg = @{}
   $msg.WindowsProductName = $infoversion.WindowsProductName
   $msg.WindowsVersion = $infoversion.WindowsVersion
   $msg.OsHardwareAbstractionLayer = $infoversion.OsHardwareAbstractionLayer
   $result.msg = $msg
+  if ($infoversion.WindowsProductName -ne $WindowsProductName)
+  {
+    Fail-Json $result "Changed WindowsProductName"
+  }
+  if ($infoversion.WindowsVersion -ne WindowsVersion)
+  {
+    Fail-Json $result "Changed WindowsVersion"
+  }
+  if ($infoversion.OsHardwareAbstractionLayer -ne OsHardwareAbstractionLayer)
+  {
+    Fail-Json $result "Changed OsHardwareAbstractionLayer"
+  }
   Exit-Json $result;
 }
 Catch {
