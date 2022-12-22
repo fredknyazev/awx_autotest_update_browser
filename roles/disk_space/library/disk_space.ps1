af942ce94671af3acdbf8ad5fc43d062a19db32f
ua.knyazev <ua.knyazev@tensor.ru>
2022-12-22 15:09:25 +0300

    disk space

---
+++ roles/disk_space/library/disk_space.ps1
@@ -0,0 +1,24 @@
+#!powershell
+
+# WANT_JSON
+# POWERSHELL_COMMON
+#!powershell
+
+$params = Parse-Args $args;
+$result = @{};
+Try {
+  $AvaliableSize = Get-Attr -obj $params -name AvaliableSize
+  $disc_size = Get-CimInstance -ClassName Win32_LogicalDisk | where caption -eq "C:" | Select-Object -Property DeviceID,FreeSpace,Size
+  $msg = @{}
+  $msg.FreeSpace = $disc_size.FreeSpace
+  $msg.Size = $disc_size.Size
+  $result.msg = $msg
+  if ($disc_size.Size -lt $AvaliableSize)
+  {
+    Fail-Json $result "Less FreeSpace Current: $msg.FreeSpace"
+  }
+  Exit-Json $result;
+}
+Catch {
+  Fail-Json $result $_.Exception.Message
+}
